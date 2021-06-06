d91a17e76384f951ccc17c22631225e907e3a3ac
Sameh Abouelsaad <samehabouelsaad@gmail.com>
2021-06-06 20:58:27 +0200

    feat: Adding extra options
    - search directories using -r or --recursive
    - counting the matched lines using -c or --count
    and now pattern accept regex instead of plan text used before

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,89 @@ use structopt::StructOpt;
 use std::io::prelude::*;
 use std::fs::File;
 use anyhow::{Context, Result};
+use regex::Regex;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt, Debug)]
 #[structopt(name = "rstGREP", about = "an alternative to grep written in Rust.")]
 struct Opt {
     /// The pattern to look for
-    pattern: String,
+    pattern: Regex,
     /// The path to the file to read
-    #[structopt(short, parse(from_os_str))]
+    #[structopt(parse(from_os_str))]
     path: PathBuf,
+    /// Show relative line number in the file
+    #[structopt(short, long = "--line-number")]
+    n: bool,
+    /// Read all files under each directory, recursively
+    #[structopt(short, long="--recursive")]
+    r: bool,
+    /// print only a count of selected lines per FILE
+    #[structopt(short, long = "--count")]
+    c: bool,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Opt::from_args();
-    let f = File::open(&args.path)
-    .with_context(|| format!("could not read file `{:?}`", &args.path))?;
+    // println!{"{:?}", &f}
+    if args.r 
+    {
+        walk_fs(&args, &args.path);
+    } else {
+        let f = File::open(&args.path)
+        .with_context(|| format!("could not read file `{:?}`", &args.path))?;
+        match_pattern_in_file(&f, &args, &args.path);
+    }
     //.expect(&format!("Could not read file {:?}", args.path)[..]);
+    Ok(())
+}
+
+fn match_pattern_in_file(f: &std::fs::File, args: &Opt, path: &std::path::PathBuf) {
     let reader = BufReader::new(f);
-    for (ln, line) in reader.lines().map(|l| l.unwrap()).enumerate() {
-        if line.contains(&args.pattern) {
-            println!("{}: {}", ln, line);
+    let mut count: u32 = 0;
+    for (ln, line) in reader.lines().enumerate() {
+        match line {
+            Ok(line) => {
+                if args.pattern.is_match(&line) {
+                    count = count + 1;
+                    if count == 1 && args.r {
+                        println!();
+                        println!("-------- File Path: {} --------", path.display());
+                    }
+                    if !args.c {
+                        if args.n {
+                            print!("{}:\t", ln+1);
+                        }
+                        println!("{}", line.trim());
+                    }
+                    
+                }
+            },
+            Err(e) => { // mostly invalid utf8 content
+                if args.r {
+                    return
+                } else {
+                    panic!("Error! {}", e);
+                }
+            },
         }
     }
-    Ok(())
+    if args.c && count > 0 {
+        println!("Count: {}", count);
+    }
+}
+
+fn walk_fs(args: &Opt, path: &std::path::PathBuf) {
+    for entry in path.read_dir().expect("read_dir call failed") {
+        if let Ok(entry) = entry {
+            if let Ok(file_type) = entry.file_type() {
+                if file_type.is_file() {
+                    let f = File::open(&entry.path()).expect(&format!("Could not read file {:?}", entry.path())[..]);
+                    match_pattern_in_file(&f, &args, &entry.path());
+                } else if file_type.is_dir() {
+                    walk_fs(&args, &entry.path());
+                }
+            }
+        }
+    }
 }
